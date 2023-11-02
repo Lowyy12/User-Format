42adb1114fa275225c2717efed087e562c7a6872
AlumnosAsir <alumnos.asir.primero@gmail.com>
2023-11-02 21:04:22 +0100

    Segundo

---
+++ sql_modificado.sql
@@ -0,0 +1,108 @@
+
+create user franciscoalal identified by uno 
+default tablespace USERS temporary tablespaces TEMP 
+quota 100k on USERS;
+
+grant rol_primero to franciscoalal;
+
+create user miguelbabo identified by uno 
+default tablespace USERS temporary tablespaces TEMP 
+quota 100k on USERS;
+
+grant rol_primero to miguelbabo;
+
+create user javierbamo identified by uno 
+default tablespace USERS temporary tablespaces TEMP 
+quota 100k on USERS;
+
+grant rol_primero to javierbamo;
+
+create user anabrpe identified by uno 
+default tablespace USERS temporary tablespaces TEMP 
+quota 100k on USERS;
+
+grant rol_primero to anabrpe;
+
+create user soukainaelbe identified by uno 
+default tablespace USERS temporary tablespaces TEMP 
+quota 100k on USERS;
+
+grant rol_primero to soukainaelbe;
+
+create user enriquefrul identified by uno 
+default tablespace USERS temporary tablespaces TEMP 
+quota 100k on USERS;
+
+grant rol_primero to enriquefrul;
+
+create user josegabe identified by uno 
+default tablespace USERS temporary tablespaces TEMP 
+quota 100k on USERS;
+
+grant rol_primero to josegabe;
+
+create user oscarholo identified by uno 
+default tablespace USERS temporary tablespaces TEMP 
+quota 100k on USERS;
+
+grant rol_primero to oscarholo;
+
+create user joselimo identified by uno 
+default tablespace USERS temporary tablespaces TEMP 
+quota 100k on USERS;
+
+grant rol_primero to joselimo;
+
+create user sergiomase identified by uno 
+default tablespace USERS temporary tablespaces TEMP 
+quota 100k on USERS;
+
+grant rol_primero to sergiomase;
+
+create user franciscomola identified by uno 
+default tablespace USERS temporary tablespaces TEMP 
+quota 100k on USERS;
+
+grant rol_primero to franciscomola;
+
+create user antoniomova identified by uno 
+default tablespace USERS temporary tablespaces TEMP 
+quota 100k on USERS;
+
+grant rol_primero to antoniomova;
+
+create user mariomual identified by uno 
+default tablespace USERS temporary tablespaces TEMP 
+quota 100k on USERS;
+
+grant rol_primero to mariomual;
+
+create user manuelposa identified by uno 
+default tablespace USERS temporary tablespaces TEMP 
+quota 100k on USERS;
+
+grant rol_primero to manuelposa;
+
+create user javierquro identified by uno 
+default tablespace USERS temporary tablespaces TEMP 
+quota 100k on USERS;
+
+grant rol_primero to javierquro;
+
+create user anatosa identified by uno 
+default tablespace USERS temporary tablespaces TEMP 
+quota 100k on USERS;
+
+grant rol_primero to anatosa;
+
+create user luisvava identified by uno 
+default tablespace USERS temporary tablespaces TEMP 
+quota 100k on USERS;
+
+grant rol_primero to luisvava;
+
+create user michellepaci identified by uno 
+default tablespace USERS temporary tablespaces TEMP 
+quota 100k on USERS;
+
+grant rol_primero to michellepaci;
